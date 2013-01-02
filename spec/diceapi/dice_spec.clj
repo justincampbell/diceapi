730a324439229fe2feeaecd03b1025e77f24419a
Justin Campbell <justin@justincampbell.me>
2013-01-02 10:21:14 -0500

    Nest describe for returned function

--- spec/diceapi/dice_spec.clj
+++ spec/diceapi/dice_spec.clj
@@ -7,19 +7,23 @@
   (it "returns a function"
     (should (fn? (die 6))))
 
-  (it "which returns 0 when given a negative"
-    (should= 0 ((die -1))))
+  (describe "returned function"
 
-  (it "which returns 0 when given 0"
-    (should= 0 ((die 0))))
+    (it "returns 0 when given a negative"
+      (should= 0 ((die -1))))
 
-  (it "which returns 1 when given 1"
-    (should= 1 ((die 1))))
+    (it "returns 0 when given 0"
+      (should= 0 ((die 0))))
 
-  (it "which rolls between 1 and the given maxiumum"
-    (let [rolls (map #(%) (repeat 1000 (die 6)))]
-      (should= 1 (reduce min rolls))
-      (should= 6 (reduce max rolls))))
+    (it "returns 1 when given 1"
+      (should= 1 ((die 1))))
+
+    (it "rolls between 1 and the given maxiumum"
+      (let [rolls (map #(%) (repeat 1000 (die 6)))]
+        (should= 1 (reduce min rolls))
+        (should= 6 (reduce max rolls))))
+
+  )
 
 )
 
