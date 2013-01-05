579128a4d7938414aecda176f0ad23f35ff547ce
Justin Campbell <justin@justincampbell.me>
2013-01-04 22:22:04 -0500

    Require leiningen 2

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://github.com/justincampbell/diceapi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.3"]]
   :plugins [[lein-ring "0.7.5"]
