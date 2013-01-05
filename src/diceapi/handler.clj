2fcbcc00398400f24a9c3695500eb4f924ce261f
Justin Campbell <justin@justincampbell.me>
2013-01-04 22:41:38 -0500

    Better route matching

--- src/diceapi/handler.clj
+++ src/diceapi/handler.clj
@@ -13,7 +13,7 @@
           "<a href=\"d20\">/d20</a><br />"
           "<a href=\"2d6\">/2d6</a>"))
 
-   (GET "/:roll" [roll]
+   (GET ["/:roll" :roll #"\d*d\d+"] [roll]
      (str "[" (join ", " (roll-string roll)) "]"))
 
    (route/not-found "<h1>Page not found</h1>")
