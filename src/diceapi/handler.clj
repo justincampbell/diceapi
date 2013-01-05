d8dde9bbb3776157add2b837ed03c21c16180fb0
Justin Campbell <justin@justincampbell.me>
2013-01-04 22:16:08 -0500

    Map url path to roll-string

--- src/diceapi/handler.clj
+++ src/diceapi/handler.clj
@@ -1,8 +1,21 @@
 (ns diceapi.handler
-  (:use compojure.core)
+  (:use compojure.core
+        [diceapi.dice :only (roll-string)]
+        [clojure.string :only (join)])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
 (defroutes app
-   (GET "/" [] "<h1>Hello World</h1>")
-   (route/not-found "<h1>Page not found</h1>"))
+
+   (GET "/" []
+     (str "<h1>diceapi</h1><br />"
+          "<a href=\"d6\">/d6</a><br />"
+          "<a href=\"d20\">/d20</a><br />"
+          "<a href=\"2d6\">/2d6</a>"))
+
+   (GET "/:roll" [roll]
+     (str "[" (join ", " (roll-string roll)) "]"))
+
+   (route/not-found "<h1>Page not found</h1>")
+
+)
